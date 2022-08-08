88cce1b1c53c9e6cc42c7277a7f4fa6adf9d2e4c
fabrizia2 <melvinrenish56@gmail.com>
2022-08-08 06:58:01 -0700

    Print is the sharpest and the strongest weapon of our party

--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+#define S_LONG 2
+#define S_SHORT 1
 
 #include <stdarg.h>
 #include <unistd.h>
